855e02baf1c8d09ead35cb96f92ba9802485c1b4
rahul kumar barnwal <rahulbarnwal1499@gmail.com>
2020-10-15 18:20:36 +0530

    insertionsort updated

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,65 +1,41 @@
-#include <iostream>
-using namespace std;
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
-}
-void insertion_sort(int *arr, int from, int to)
-{
-	int key = 0;
-	int j = 0;
-	
-	for(int i = from + 1; i <= to; i++)
-	{
-		key = arr[i];
-		j = i - 1;
-		while(key < arr[j] && j >= from)
-		{
-			arr[j + 1] = arr[j];
-			j--;
-		}
-		arr[j+1] = key;
-	}
-}
+#include <bits/stdc++.h> 
+using namespace std; 
+  
 
-
-void doiSort(int buf[], int len, int maxl)
-{
-	if (len >= maxl)
-	{
-		return;
-	}
-	if (len == 1)
-	{
-		if (buf[0] > buf[1])
-		{
-			//cout << " Shift 1" << endl;
-			int r = buf[1];
-			buf[1] = buf[0];
-			buf[0] = r;
-		}
-		doiSort(buf, 2, maxl);
-	}
-	else
-	{
-		int m = buf[len];
-		for (int i = 0; i < len; i++)
-		{
-			if (buf[i] > m)
-			{
-				//cout << "insert " << buf[i] << "->" << buf[len] << " " << endl;
-				for (int j = len; j > i; j--)
-				{
-					int t = buf[j];
-					buf[j] = buf[j - 1];
-					buf[j - 1] = t;
-				}
-				break;
-			}
-		}
-
-		doiSort(buf, len + 1, maxl);
-	}
-	return;
-}
+void InsertionSort(int list[], int n)  
+{  
+    int i, flag, j;  
+    for (i = 1; i < n; i++) 
+    {  
+        flag = list[i];  
+        j = i - 1;  
+  
+        
+        while (j >= 0 && list[j] >flag) 
+        {  
+            list[j + 1] = list[j];  
+            j = j - 1;  
+        }  
+        list[j + 1] = flag;  
+    }  
+}  
+  
+  
+void PrintArray(int list[], int n)  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        cout << list[i] << " ";  
+    cout << endl; 
+}  
+int main()  
+{  
+    int list[] = { 2, 1, 3, 15, 26 };  
+    int n = sizeof(list) / sizeof(list[0]);  
+  
+    InsertionSort(list, n);  
+    PrintArray(list, n);  
+  
+    return 0;  
+}  
+  

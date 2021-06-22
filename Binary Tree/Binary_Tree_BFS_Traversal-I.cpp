e3aee5e091ffcf5677f6083bb5a6a031b2197538
Ankit Kaushal <55594118+ankit-kaushal@users.noreply.github.com>
2021-06-22 23:58:28 +0530

    Binary Tree Codes

---
+++ Binary Tree/Binary_Tree_BFS_Traversal-I.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+
+    if(d==-1){
+        return NULL;
+    }
+    node * root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+void print(node *root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise, print root first followed by subtrees(children)
+    cout<<root->data<<" ";
+    print(root->left);
+    print(root->right);
+}
+
+void printIn(node*root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise Left Root Right
+    printIn(root->left);
+    cout<<root->data<<" ";
+    printIn(root->right);
+}
+
+void printPost(node*root){
+    if(root==NULL){
+        return;
+    }
+    printPost(root->left);
+    printPost(root->right);
+    cout<<root->data<<" ";
+}
+
+int height(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    int ls = height(root->left);
+    int rs = height(root->right);
+    return max(ls,rs) + 1;
+
+}
+
+void printKthLevel(node*root,int k){
+    if(root==NULL){
+        return;
+    }
+    if(k==1){
+        cout<<root->data<<" ";
+        return;
+    }
+    printKthLevel(root->left,k-1);
+    printKthLevel(root->right,k-1);
+    return;
+
+}
+
+void printAllLevels(node*root){
+    int H = height(root);
+
+    for(int i=1;i<=H;i++){
+        printKthLevel(root,i);
+        cout<<endl;
+    }
+    return;
+}
+
+int count(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1 + count(root->left) + count(root->right);
+}
+
+void bfs(node *root){
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        node* f = q.front();
+        cout<<f->data<<",";
+        q.pop();
+
+        if(f->left){
+            q.push(f->left);
+        }
+        if(f->right){
+            q.push(f->right);
+        }
+    }
+    return;
+}
+
+
+int main(){ 
+    node* root = buildTree();
+    //cout<<count(root)<<endl;
+    //printAllLevels(root);
+    bfs(root);
+
+return 0;
+}
+
+/*
+Input ->
+8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+
+Output ->
+8,10,3,1,6,14,9,7,13,
+
+*/
+
+
+
+
+
+
+
+
+

4b5452516f115d851e22a7aaabd67c33f021636f
Josal <52382282+J0SAL@users.noreply.github.com>
2021-10-25 23:09:59 +0530

    added a program to print catalan program
    Added a C++ program to print catalan numbers in C++

---
+++ catalan_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int catalan(int n){
+	if(n<=1)
+		return 1;
+	int res=0;
+	for(int i=0; i<n; i++){
+		res+=catalan(i) * catalan(n-i-1);
+	}
+	return res;
+}
+int main(){
+	for(int i=0; i<10; i++){
+		cout<<catalan(i)<<" ";
+	}
+	return 0;
+}
